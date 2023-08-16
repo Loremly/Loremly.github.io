eb616f69477af19815aacb6f9143e1b3cfc3f9a3
Lorem Sapphire <loremly@pm.me>
2023-08-16 14:58:44 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind()],
   site: 'https://loremly.github.io',
   base: '/',
